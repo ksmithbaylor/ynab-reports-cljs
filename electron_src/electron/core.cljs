8db05b2605522affd2beb97e6ee445c1e1f7f408
Kevin Smith <ksmithbaylor@gmail.com>
2017-01-28 15:08:08 -0500

    Open devtools and print message when electron starts

--- electron_src/electron/core.cljs
+++ electron_src/electron/core.cljs
@@ -12,6 +12,8 @@
                                   :height 600})))
   ; Path is relative to the compiled js file (main.js in our case)
   (.loadURL @main-window (str "file://" js/__dirname "/public/index.html"))
+  (.openDevTools (.-webContents @main-window))
+  (.log js/console "App initialized!")
   (.on @main-window "closed" #(reset! main-window nil)))
 
 (.on app "window-all-closed" #(when-not (= js/process.platform "darwin")
