72c80d001c94e9ec306229a96673505775bde9cb
Kevin Smith <ksmithbaylor@gmail.com>
2017-04-26 02:22:45 -0400

    Reorganize the entry point

--- ui_src/ui/core.cljs
+++ ui_src/ui/core.cljs
@@ -6,18 +6,17 @@
             [ui.subs :as subs]
             [ui.views :as views]))
 
-(defn dev-setup []
-  (when goog.DEBUG
-    (enable-console-print!)
-    (enable-re-frisk!)
-    (println "dev mode on")))
+; This flag gets turned on during development
+(when goog.DEBUG
+  (enable-console-print!)
+  (enable-re-frisk!)
+  (println "dev mode on"))
 
-(defn init []
-  (dev-setup)
-  (rf/dispatch-sync [:initialize])
-  (rf/clear-subscription-cache!)
-  (r/render
-    [views/main]
-    (.getElementById js/document "app-container")))
+; Initialize the re-frame app
+(rf/dispatch-sync [:initialize])
+(rf/clear-subscription-cache!)
 
-(init)
+; Render the main component
+(r/render
+  [views/main]
+  (.getElementById js/document "app-container"))
