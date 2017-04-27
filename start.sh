0244a15a2f66688f96b84877e0b539e841d72b37
Kevin Smith <ksmithbaylor@gmail.com>
2017-04-27 12:09:24 -0400

    Convert CSS over to LESS, and allow theming of antd

--- start.sh
+++ start.sh
@@ -2,5 +2,9 @@
 
 tmux split-window -h -c '#{pane_current_path}'
 tmux send-keys 'C-l' 'yarn run electron' 'C-m'
+
+tmux split-window -c '#{pane_current_path}'
+tmux send-keys 'C-l' 'yarn run less' 'C-m'
+
 tmux select-pane -t :.+
 tmux send-keys 'C-l' 'yarn run figwheel' 'C-m'
