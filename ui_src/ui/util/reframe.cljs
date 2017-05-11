ed5f9320e2bca5675e1cc42380a1f5681f1db242
Kevin Smith <ksmithbaylor@gmail.com>
2017-05-11 15:03:42 -0400

    Create custom debug interceptor

--- ui_src/ui/util/reframe.cljs
+++ ui_src/ui/util/reframe.cljs
@@ -1,17 +1,12 @@
 (ns ui.util.reframe
-  (:require [re-frame.core :as rf :refer [after debug]]
+  (:require [re-frame.core :as rf :refer [after]]
+            [re-frame.interceptor :refer [->interceptor get-effect get-coeffect]]
+            [re-frame.loggers :refer [console]]
             [re-frame.std-interceptors :refer [db-handler->interceptor]]
+            [clojure.data :as data]
             [ui.db :refer [initial-state]]
             [ui.fs.persistence :refer [write-to-disk]]
             [cljs.spec :as s]))
-
-(def draw-line
-  (db-handler->interceptor
-    (fn [db _]
-      (.log js/console
-        (apply str "%c" (repeat 80 " "))
-        "border-bottom: 5px solid green")
-      db)))
 
 (defn check-and-throw
   "throw an exception if db doesn't match the spec"
@@ -26,9 +21,43 @@
       (write-to-disk db
         #(when % (.error js/console %))))))
 
+(def debug
+  (->interceptor
+    :id     :debug
+    :before (fn debug-before
+              [context]
+              (let [event (get-coeffect context :event)
+                    event-name (first event)]
+                (js/console.groupCollapsed
+                  (str "%c" event-name (apply str (repeat (- 80 (count (str event-name))) " ")))
+                  "color: #3e4147; border-top: 3px solid #0082D0")
+                (js/console.groupCollapsed "event")
+                (console :log event)
+                (console :groupEnd))
+              context)
+    :after  (fn debug-after
+              [context]
+              (let [event   (get-coeffect context :event)
+                    orig-db (get-coeffect context :db)
+                    new-db  (get-effect   context :db ::not-found)]
+                (if (= new-db ::not-found)
+                  (console :log "No db changes")
+                  (let [[only-before only-after] (data/diff orig-db new-db)
+                        db-changed?    (or (some? only-before) (some? only-after))]
+                    (if db-changed?
+                      (do (js/console.groupCollapsed "diff")
+                          (console :log "only before:" only-before)
+                          (console :log "only after :" only-after)
+                          (console :groupEnd))
+                      (console :log "no app-db changes caused by:" event))))
+                (console :groupEnd)
+                ; (console :log
+                ;   (apply str "%c" (repeat 80 " "))
+                ;   "border-top: 3px solid green")
+                context))))
+
 (def standard-middleware
-  [draw-line
-   check-db-spec
+  [check-db-spec
    persist!
    debug])
 
