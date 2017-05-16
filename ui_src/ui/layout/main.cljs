75f687217b8bc3fce20d0e33caaa6043584f7602
Kevin Smith <ksmithbaylor@gmail.com>
2017-05-15 21:08:14 -0400

    Fix figwheel reloading again

--- ui_src/ui/layout/main.cljs
+++ ui_src/ui/layout/main.cljs
@@ -5,12 +5,12 @@
             [ui.pages.progress-bars :refer [progress-bars]]))
 
 (def routes
-  {:summary summary
-   :preferences preferences
-   :progress-bars progress-bars})
+  {:summary [#'summary]
+   :preferences [#'preferences]
+   :progress-bars [#'progress-bars]})
 
 (defn main []
   (let [page @(rf/subscribe [:page])
         component (routes page)]
     [:main.ui__main
-      [component]]))
+      component]))
